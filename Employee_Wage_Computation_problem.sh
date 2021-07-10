890c38490cac727f13c0fd694bd25e54bb1a854a
moshtab <mdmohsin41439@gmail.com>
2021-07-10 15:27:40 +0530

    adding part time

--- Employee_Wage_Computation_problem.sh
+++ Employee_Wage_Computation_problem.sh
@@ -1,12 +1,15 @@
 ispresent=0
 isabsent=1
+isparttime=2
 wagePerHr=20
-num=$((RANDOM%2))
+num=$((RANDOM%3))
 case $num in
 	0) workinghr=8
 	   echo "emp is present" ;;
 	1) workinghr=0
 	   echo "emp is absent"  ;;
+	2) workinghr=4
+	   echo "emp is part time"
 esac
 salary=$((wagePerHr*workinghr))
 echo "Daily employee wage is : " $salary
