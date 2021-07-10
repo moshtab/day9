e8e3b600a0a79bb686a6c687c068dba97b3badb5
moshtab <mdmohsin41439@gmail.com>
2021-07-10 17:28:12 +0530

    storing wage

--- Employee_Wage_Computation_problem.sh
+++ Employee_Wage_Computation_problem.sh
@@ -1,15 +1,18 @@
+ispresent=0
+isparttime=1
+isabsent=2
 wagePerHr=20
-function workinghr()
-{
-      case $1 in
+for ((i=0;i<20;i++))
+do
+      num=$((RANDOM%3))
+      case $num in
 	 0) workinghr=8 ;;
-	 1) workinghr=0 ;;
-	 2) workinghr=4 ;;
+	 1) workinghr=4 ;;
+	 2) workinghr=0 ;;
       esac
-      echo $workinghr
-}
-workinghr="$(workinghr $((RANDOM%3)))"
-salary=$(((wagePerHr*workinghr)))
-echo "workinghr is " $workinghr
+      salary=$((salary+(wagePerHr*workinghr)))
+      array[i]=$((wagePerHr*workinghr))
+done
+echo ${array[@]}
 echo "employee wage for month is : " $salary
 
