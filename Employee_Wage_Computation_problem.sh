3552b6489015d91aa9a77dd1a44e53de422cb95e
moshtab <mdmohsin41439@gmail.com>
2021-07-10 16:33:07 +0530

    function

--- Employee_Wage_Computation_problem.sh
+++ Employee_Wage_Computation_problem.sh
@@ -1,27 +1,15 @@
-ispresent=0
-isabsent=1
-isparttime=2
 wagePerHr=20
-workinghr=0
-workingday=0
-while [ $workinghr -lt 100 ] && [ $workingday -lt 20 ]
-do
-   for ((i=0;i<20;i++))
-   do
-      num=$((RANDOM%3))
-      case $num in
-	 0) workinghr=8
-	    workingday=$((workingday+1))
-	   echo "emp is present" ;;
-	 1) workinghr=0
-            workingday=$((workingday+1))
-	   echo "emp is absent"  ;;
-	 2) workinghr=4
-	    workingday=$((workingday+1))
-	   echo "emp is part time" ;;
+function workinghr()
+{
+      case $1 in
+	 0) workinghr=8 ;;
+	 1) workinghr=0 ;;
+	 2) workinghr=4 ;;
       esac
-      salary=$((salary+(wagePerHr*workinghr)))
-   done
-done
+      echo $workinghr
+}
+workinghr="$(workinghr $((RANDOM%3)))"
+salary=$(((wagePerHr*workinghr)))
+echo "workinghr is " $workinghr
 echo "employee wage for month is : " $salary
 
