a03e84ba02a09c2ecbcdb6fff44109e1aba53276
moshtab <mdmohsin41439@gmail.com>
2021-07-10 16:07:26 +0530

    wage condition 100hrs and 20days

--- Employee_Wage_Computation_problem.sh
+++ Employee_Wage_Computation_problem.sh
@@ -2,18 +2,26 @@ ispresent=0
 isabsent=1
 isparttime=2
 wagePerHr=20
-for ((i=0;i<20;i++))
+workinghr=0
+workingday=0
+while [ $workinghr -lt 100 ] && [ $workingday -lt 20 ]
 do
-   num=$((RANDOM%3))
-   case $num in
-	0) workinghr=8
+   for ((i=0;i<20;i++))
+   do
+      num=$((RANDOM%3))
+      case $num in
+	 0) workinghr=8
+	    workingday=$((workingday+1))
 	   echo "emp is present" ;;
-	1) workinghr=0
+	 1) workinghr=0
+            workingday=$((workingday+1))
 	   echo "emp is absent"  ;;
-	2) workinghr=4
-	   echo "emp is part time"
-   esac
-   salary=$((salary+(wagePerHr*workinghr)))
+	 2) workinghr=4
+	    workingday=$((workingday+1))
+	   echo "emp is part time" ;;
+      esac
+      salary=$((salary+(wagePerHr*workinghr)))
+   done
 done
 echo "employee wage for month is : " $salary
 
